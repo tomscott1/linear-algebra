09ea9948463469d6a2b634d9ef7790e0fb24363c
Tom Scott <Tom Scott@au.aap.ad.pwcinternal.com>
2016-08-23 14:05:05 +1000

    Add magnitutude and direction functions

--- la-functions.py
+++ la-functions.py
@@ -1,6 +1,6 @@
 import math
 
-# my code
+# my initial code
 
 def addVector(first_v, second_v):
     result_x = float(first_v[0]) + float(second_v[0])
@@ -31,7 +31,21 @@ class Vector(object):
     def __init__(self, coordinates):
         self.coordinates = coordinates
 
-    # Udacity code
+''' Udacity __init__ code
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+'''
+
+# Udacity code
 
     def plus(self, v):
         new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
@@ -45,26 +59,38 @@ class Vector(object):
         new_coordinates = [c*x for x in self.coordinates]
         return Vector(new_coordinates)
 
-    # my code
+# my code
 
     def magnitude(self):
         coordinates_squared = [x**2 for x in self.coordinates]
         return math.sqrt(sum(coordinates_squared))
 
     def direction(self):
+
+        new_coordinates = [x*(1./self.magnitude()) for x in self.coordinates]
+        return Vector(new_coordinates)
+
+''' udacity code for normalization
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+
+        except ZeroDivisionError:
+            raise Exception('Cannot normalise the zero vector')   
+'''
         
-
-    def __len__(self)
+    def __len__(self):
         return(len(self.coordinates))
-
     
-    # udacity code
+# udacity code
 
     def __str__(self):
         return 'Vector:{}'.format(self.coordinates)
 
     def __eq__(self,v):
         return self.coordinates == v.coordinates
+
+# print answers
 
 v = Vector([8.218,-9.341])
 w = Vector([-1.129,2.111])
@@ -78,8 +104,14 @@ v = Vector([1.671,-1.012,-0.318])
 print v.times_scalar(7.41)
 
 v = Vector([-0.221,7.437])
-print v.magnitude()
+print round(v.magnitude(),3)
 
-v = Vector([8.813,-1.331,-6.247)]
-print v.magnitude()
+v = Vector([8.813,-1.331,-6.247])
+print round(v.magnitude(),3)
+
+v = Vector([5.581,-2.136])
+print v.direction()
+
+v = Vector([1.996,3.108,-4.554])
+print v.direction()
 
